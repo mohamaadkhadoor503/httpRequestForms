3d47e101dd3a66b2d650ca377bce94becee5a5ea
mohamad.khadoor <mohamad.khadoor@hiast.edu.sy>
2023-04-11 20:22:19 +0300

    forms in csharp

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.writeAnythingTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(199, 96);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 0;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(399, 96);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(79, 23);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // writeAnythingTextBox
+            // 
+            this.writeAnythingTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.writeAnythingTextBox.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.writeAnythingTextBox.Location = new System.Drawing.Point(199, 24);
+            this.writeAnythingTextBox.Name = "writeAnythingTextBox";
+            this.writeAnythingTextBox.Size = new System.Drawing.Size(267, 30);
+            this.writeAnythingTextBox.TabIndex = 2;
+            this.writeAnythingTextBox.Text = "Write anything";
+            this.writeAnythingTextBox.Enter += new System.EventHandler(this.writeAnythingTextBox_Enter);
+            this.writeAnythingTextBox.Leave += new System.EventHandler(this.writeAnythingTextBox_Leave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.writeAnythingTextBox);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.searchButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox writeAnythingTextBox;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+       
+       
+
+        private void writeAnythingTextBox_Enter(object sender, EventArgs e)
+        {
+
+            if (writeAnythingTextBox.Text == "Write anything")
+            {
+                writeAnythingTextBox.Text = "";
+                writeAnythingTextBox.ForeColor = Color.Black;
+            }
+
+        }
+
+        private void writeAnythingTextBox_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(writeAnythingTextBox.Text))
+            {
+                writeAnythingTextBox.ForeColor = Color.Gray;
+                writeAnythingTextBox.Text = "Write anything";
+            }
+        }
+
+       
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text File|*.txt";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+                string query = writeAnythingTextBox.Text;
+      
+                string url = "https://www.google.com/search?q=" + query;
+                //I used Google server instead of Yahoo because it is the reason for the exceptions
+
+
+                try
+                {
+                    // Create a web request
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+                    // Get the response
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                    StreamReader streamReader = new StreamReader(response.GetResponseStream());
+                    string result = streamReader.ReadToEnd();
+
+                    // Save the result to a file
+                    File.WriteAllText(filePath, result);
+
+                    MessageBox.Show("Result saved to " + filePath);
+                }
+                catch (WebException ex)
+                {
+                    if (ex.Status == WebExceptionStatus.TrustFailure)
+                    {
+                        // Handle SSL/TLS trust failure
+                        MessageBox.Show("Could not establish trust relationship for the SSL/TLS secure channel. Please check your network connection and try again.");
+                    }
+                    else
+                    {
+                        // Handle other web exceptions
+                        MessageBox.Show("An error occurred while trying to make the web request: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+           string query = writeAnythingTextBox.Text;
+            
+            string url = "https://www.google.com/search?q=" + query;
+            //I used Google server instead of Yahoo because it is the reason for the exceptions
+
+
+
+            try
+            {
+        // Create a web request
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+        // Get the response
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        StreamReader streamReader = new StreamReader(response.GetResponseStream());
+        string result = streamReader.ReadToEnd();
+
+        // Display the result
+        MessageBox.Show(result);
+    }
+    catch (WebException ex)
+    {
+        if (ex.Status == WebExceptionStatus.TrustFailure)
+        {
+            // Handle SSL/TLS trust failure
+            MessageBox.Show("Could not establish trust relationship for the SSL/TLS secure channel. Please check your network connection and try again.");
+        }
+        else
+        {
+            // Handle other web exceptions
+            MessageBox.Show("An error occurred while trying to make the web request: " + ex.Message);
+        }
+    }
+        }
+    }
+}
